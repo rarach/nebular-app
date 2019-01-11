597dd4e5396a433b583057df5b250885966ee1fd
rarach <martin.mihalovic@post.com>
2019-01-11 13:16:09 +0100

    Added E2E test run in TravisCI with Headless Chrome

---
+++ e2e/protractor-ci.conf.js
@@ -0,0 +1,12 @@
+//Protractor config; special version for Travis CI to run browser in headless mode
+
+const config = require('./protractor.conf').config;
+
+config.capabilities = {
+  browserName: 'chrome',
+  chromeOptions: {
+    args: ['--headless', '--no-sandbox']
+  }
+};
+
+exports.config = config;
