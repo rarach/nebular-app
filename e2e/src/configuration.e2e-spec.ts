2d3bf461cf2f92619069b0ec0f1d929427e47e5c
rarach <martin.mihalovic@post.com>
2019-09-03 23:02:48 +0200

    e2e tests fixed

--- e2e/src/configuration.e2e-spec.ts
+++ e2e/src/configuration.e2e-spec.ts
@@ -8,7 +8,7 @@ describe('Configuration page', () => {
         browser.get('/configuration');
         expect(browser.getTitle()).toBe('Nebular - Configuration');
     });
-    it("finds all relevant anchors for asset code 'USD'", () => {
+    it("finds all relevant anchors for asset code 'USD'", () => {   //TODO: "finds and adds new custom asset"
         //Actual page being tested
         browser.get("/configuration");
 
@@ -26,8 +26,6 @@ describe('Configuration page', () => {
         expect(anchorRow1.getText()).toBe("USD-www.anchorusd.com Add");
         const anchorImage1 = anchorRow1.element(by.css("img.asset-icon"));
 
-        if (!browser.ignoreSynchronization) throw new Error("blah!!!");
-
 
 
 /*TODO: this is a preferred style, but doesn't work here for some reason :-(
@@ -40,23 +38,27 @@ browser.wait(() => {
 
 
 
+        //TODO: verify that the two USD anchors are not among user's custom assets already. Add them and check that.
         browser.ignoreSynchronization = false;
     });
 
-    it("Contains list of custom assets saved by user", () => {
+    it("contains list of custom assets saved by user", () => {     //TODO: "removes user's custom asset"
         //Trick: go to invalid page to load the web without calling the dependent services
         browser.get("/no-such-page");
         browser.manage().addCookie({
             name: "ass",
-            value: "RUB-GUBERNIA2929292929292929,UHH-GBEDEDEDEDE,BTC-GBSTRONGHODL455519K7YY,zzzzz-GCCP"
+            value: "RUB|GUBERNIA2929292929292929|noSuch.ru|https://noSuch.ru/rub.ico,"+
+                   "UHH|GBEDEDEDEDE|GBE...DEDEDE|,"+
+                   "BTC|GBSTRONGHODL455519K7YY|GBSTR...K7YY|null,"+
+                   "zzzzz|GCCP|example.org|./assets/images/asset_icons/unknown.png"
         });
         browser.get("/configuration");
 
-        const assetRows = element.all(by.css("#customAssetsList div.customItemRow"));
+        const assetRows = element.all(by.css("tr.asset-item"));
         expect(assetRows.count()).toBe(4);
-        expect(assetRows.get(0).getText()).toBe("RUB-GUBERNIA29292929... (GUBERNIA2929292929292929)\nremove");
-        expect(assetRows.get(1).getText()).toBe("UHH-GBEDEDEDEDE... (GBEDEDEDEDE)\nremove");
-        expect(assetRows.get(2).getText()).toBe("BTC-GBSTRONGHODL4555... (GBSTRONGHODL455519K7YY)\nremove");
-        expect(assetRows.get(3).getText()).toBe("zzzzz-GCCP... (GCCP)\nremove");
+        expect(assetRows.get(0).getText()).toBe("RUB-noSuch.ru Remove");
+        expect(assetRows.get(1).getText()).toBe("UHH-GBE...DEDEDE Remove");
+        expect(assetRows.get(2).getText()).toBe("BTC-GBSTR...K7YY Remove");
+        expect(assetRows.get(3).getText()).toBe("zzzzz-example.org Remove");
     });
 });
