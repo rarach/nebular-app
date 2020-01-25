bcf4fa3694ef69ad44054597049ef03e45022057
Martin M <martin.mihalovic@siemens-healthineers.com>
2020-01-25 22:47:58 +0100

    Hide market thumbnail action buttons on My Exchanges while dragging. Fixes #50

--- src/app/live-trades/live-trade-item.spec.ts
+++ src/app/live-trades/live-trade-item.spec.ts
@@ -1,5 +1,6 @@
 import { HorizonRestServiceStub } from "./live-trades.component.spec";
 import { LiveTradeItem } from "./live-trade-item";
+import { Trade } from "../model/trade.model";
 
 
 describe("LiveTradeItem", () => {
@@ -9,6 +10,27 @@ describe("LiveTradeItem", () => {
         expect(item.linkText).toBe("1 XLM for 3 ASDF");
         expect(item.linkHref).toBe("/exchange/XLM/ASDF-GASDF");
         expect(item.note).toBe(" (price 0.0081 ASDF)");
-        expect(item.isEven).toBe(true)
+        expect(item.isEven).toBe(true);
+    });
+
+    it("should create instance with counter_asset_code defaulting to XLM if it's null", () => {
+        const trade: Trade = {
+            id: "trade-01",
+            base_is_seller: false,
+            base_asset_type: "alphanum4",
+            base_asset_code: "NGN",
+            base_asset_issuer: "GDDExchange",
+            base_amount: "422.0",
+            counter_asset_type: null,
+            counter_asset_code: null,
+            counter_amount: "16522.1007",
+            price: { n: 1, d: 707.0707 }
+        };
+        const item = new LiveTradeItem(trade, false);
+        expect(item.actionName).toBe("Bought ");
+        expect(item.linkText).toBe("422 NGN for 16522.101 XLM");
+        expect(item.linkHref).toBe("/exchange/NGN-GDDExchange/XLM");
+        expect(item.note).toBe(" (price 0.001414 XLM)");
+        expect(item.isEven).toBe(false);
     });
 });
