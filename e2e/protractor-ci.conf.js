2324cd13f3fe0fa7e7bc1f7dae0e31d0790e6dd6
rarach-at-gitKraken <junk.account789@gmail.com>
2020-03-05 19:46:48 +0100

    Updated chromedriver version for TravisCI

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -9,6 +9,6 @@ config.capabilities = {
   }
 };
 //NOTE: this needs to be updated with each new chrome/chromedriver/Travis_distro
-config.chromeDriver = '../node_modules/webdriver-manager/selenium/chromedriver_80.0.3987.16',
+config.chromeDriver = '../node_modules/webdriver-manager/selenium/chromedriver_80.0.3987.106',
 
 exports.config = config;
--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -20,7 +20,7 @@ exports.config = {
     print: function() {}
   },
   //NOTE: this needs to be updated with each new chrome(driver)
-  chromeDriver: '../node_modules/webdriver-manager/selenium/chromedriver_80.0.3987.16.exe',
+  chromeDriver: '../node_modules/webdriver-manager/selenium/chromedriver_80.0.3987.106.exe',
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
