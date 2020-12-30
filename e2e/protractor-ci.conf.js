457b360573f4ba170c938ae90a7ffa0f0f62614e
rarach-at-gitKraken <junk.account789@gmail.com>
2020-12-30 16:50:31 +0100

    Add e2e tests execution to PROD pipeline

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -9,6 +9,6 @@ config.capabilities = {
   }
 };
 //NOTE: this needs to be updated with each new chrome/chromedriver/Travis_distro
-config.chromeDriver = '../node_modules/webdriver-manager/selenium/chromedriver_87.0.4280.88',
+//STILL NEEDED IN GITHUB ACTIONS?  config.chromeDriver = '../node_modules/webdriver-manager/selenium/chromedriver_87.0.4280.88',
 
 exports.config = config;
