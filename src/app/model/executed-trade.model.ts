9c38be5805bfa7c594d445ed68a6dfe2dfc7efbd
Martin M <martin.mihalovic@siemens-healthineers.com>
2021-11-07 18:42:31 +0100

    Support liquidity pool buyers/sellers in trade history

--- src/app/model/executed-trade.model.ts
+++ src/app/model/executed-trade.model.ts
@@ -1,10 +1,29 @@
 export class ExecutedTrade {
+    public readonly seller: string;
+    public readonly buyer: string;
+    public readonly isPoolTrade: boolean = false;
+
     constructor(public readonly time: Date,
                 public readonly tradeType: string,
                 public readonly price: number,
                 public readonly amount: number,
-                public readonly sellingAccount: string,
-                public readonly buyingAccount: string,
+                sellingAccount: string,
+                buyingAccount: string,
                 public isLastOfDay: boolean = false) {
+        if (sellingAccount) {
+            this.seller = sellingAccount.substring(0, 8) + '...' + sellingAccount.substring(48);
+        }
+        else {
+            this.seller = '(liq. pool)';
+            this.isPoolTrade = true;
+        }
+
+        if (buyingAccount) {
+            this.buyer = buyingAccount.substring(0, 8) + '...' + buyingAccount.substring(48);
+        }
+        else {
+            this.buyer = '(liq. pool)';
+            this.isPoolTrade = true;
+        }
     }
 }
