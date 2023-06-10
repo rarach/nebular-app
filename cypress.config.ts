0965dcf9a95ea50ec5ae257c5ff38b9ef452eb39
Martin M <martin.mihalovic@siemens-healthineers.com>
2023-06-10 22:04:21 +0200

    Debug CD pipeline: add e2e video recording to artifacts, cleanup

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,12 +7,4 @@ export default defineConfig({
   },
   
   
-  component: {
-    devServer: {
-      framework: 'angular',
-      bundler: 'webpack',
-    },
-    specPattern: '**/*.cy.ts'
-  }
-  
 })
