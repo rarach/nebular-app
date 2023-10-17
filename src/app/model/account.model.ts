b8206b61bc29d25f8f6e4cd0afd3986abbdf646c
ERNI\mimr <martin.mihalovic@betterask.erni>
2023-10-17 21:53:16 +0200

    Fix unit tests

--- src/app/model/account.model.ts
+++ src/app/model/account.model.ts
@@ -16,6 +16,7 @@ export class Account {
 export const KnownAccounts = {
     "Firefly" : new Account("GCNY5OXYSY4FKHOPT2SPOQZAOEIGXB5LBYW3HVU3OWSTQITS65M5RCNY", "fchain.io"),
     "Interstellar" : new Account("GCNSGHUCG5VMGLT5RIYYZSO7VQULQKAJ62QA33DBC5PPBSO57LFWVV6P", "interstellar.exchange"),
+    "Mobius" : new Account("GA6HCMBLTZS5VYYBCATRBRZ3BZJMAFUDKYYF6AH6MVCMGWMRDNSWJPIH", "mobius.network"),
     "RippleFox" : new Account("GAREELUB43IRHWEASCFBLKHURCGMHE5IF6XSE7EXDLACYHGRHM43RFOX", "ripplefox.com"),
     "SmartLands": new Account("GCKA6K5PCQ6PNF5RQBF7PQDJWRHO6UOGFMRLK3DYHDOI244V47XKQ4GP", "smartlands.io"),
     "Stronghold" : new Account("GDSTRSHXHGJ7ZIVRBXEYE5Q74XUVCUSEKEBR7UCHEUUEK72N7I7KJ6JH", "stronghold.co"),
