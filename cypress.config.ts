6d8b7ee45dcd8893af7d1997e658ec842a728d37
Martin <martin.mihalovic@post.com>
2023-10-19 15:17:40 +0200

    Cypress types in vscode (#122)
    * Minor code cleanup. Remove dead asset SLT from common assets
    
    * Fix Cypress types resolution in VS Code
    
    ---------
    
    Co-authored-by: ERNI\mimr

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   
   e2e: {
-    'baseUrl': 'http://localhost:4200'
-  },
-  
-  
+    'baseUrl': 'http://localhost:4200',
+    specPattern: "**/*.cy.ts"
+  }
 })
