b0807256d83e4917f13b2738b6c8b8590acebbdb
Martin <martin.mihalovic@post.com>
2024-10-30 16:18:41 +0100

    Filter assets with full text filter on their title (#143)
    Co-authored-by: RBAMOUSER\mihalovm <martin.mihalovic@contractors.roche.com>

--- src/app/model/asset-statistics.spec.ts
+++ src/app/model/asset-statistics.spec.ts
@@ -5,11 +5,11 @@ import { Constants } from './constants';
 import { HorizonRestService } from '../services/horizon-rest.service';
 import { IssuerConfiguration } from './toml/issuer-configuration';
 import { TomlConfigService } from '../services/toml-config.service';
+import { TomlAsset } from './toml/toml-asset';
 
 describe('AssetStatistics', () => {
-  //TODO: The positive case of constructor
-
   it('#constructor sets asset icon to "UNKNONW" when it`s unavailable', () => {
+    //arrange
     const tomlConfigServiceStub = {
       getIssuerConfig(tomlFileUrl: string) : Observable<IssuerConfiguration> {
         const issuerConfig = {
@@ -19,35 +19,83 @@ describe('AssetStatistics', () => {
       }
     } as TomlConfigService;
 
-    const assetStats = new AssetStatistics(new HorizonRestServiceStub() as HorizonRestService, tomlConfigServiceStub, 'ABC', 'GBDEV3333');
+    //act
+    const assetStats = new AssetStatistics(new HorizonRestServiceStub() as HorizonRestService, tomlConfigServiceStub, new Set(), 'ABC', 'GBDEV3333');
 
+    //assert
     expect(assetStats.assetTitle).toBe('ABC-asdf.com');
     expect(assetStats.assetIcon).toBe('./assets/images/asset_icons/unknown.png');
   });
 
   it('#constructor sets asset icon to "unknown" when unable to download it', () => {
+    //arrange
     const tomlConfigServiceStub = {
       getIssuerConfig(tomlFileUrl: string) : Observable<IssuerConfiguration> {
         return throwError(() => new Error('Cannot read this stellar.toml'));
       }            
     } as TomlConfigService;
 
-    const assetStats = new AssetStatistics(new HorizonRestServiceStub() as HorizonRestService, tomlConfigServiceStub, 'XYZ', 'GBDEV3333');
+    //act
+    const assetStats = new AssetStatistics(new HorizonRestServiceStub() as HorizonRestService, tomlConfigServiceStub, new Set(), 'XYZ', 'GBDEV3333');
 
+    //assert
     expect(assetStats.assetIcon).toBe(Constants.UNKNOWN_ASSET_IMAGE);
   });
 
   it('#constructor sets asset`s title and icon only by ID when config file URL is unknown', () => {
+    //arrange
     const horizonServiceStub = {
       getIssuerConfigUrl(assetCode: string, assetIssuer: string) : Observable<string|null> {
         return of(null);
       }          
     } as HorizonRestService;
 
-    const assetStats = new AssetStatistics(horizonServiceStub, null!, 'OOO', 'GBUYYBXWCLT2MOSSHRFCKMEDFOVSCAXNIEW424GLN666OEXHAAWBDYMX');
+    //act
+    const assetStats = new AssetStatistics(horizonServiceStub, null!, new Set(), 'OOO', 'GBUYYBXWCLT2MOSSHRFCKMEDFOVSCAXNIEW424GLN666OEXHAAWBDYMX');
 
+    //assert
     expect(assetStats.assetTitle).toBe('OOO-GBUYYBXW...AAWBDYMX');
     expect(assetStats.assetIcon).toBe('./assets/images/asset_icons/OOO.png');
+  });
+
+  it('#constructor sets "hidden" to FALSE when asset full title not found in filter-list', () => {
+    //arrange
+    const tomlConfigServiceStub = {
+      getIssuerConfig(tomlFileUrl: string) : Observable<IssuerConfiguration> {
+        const issuerConfig = {
+          currencies: [ {code: 'USDX', issuer: 'GAMIFED', image: '/usdx.jpg' } as TomlAsset ]
+        } as unknown as IssuerConfiguration;
+        return of(issuerConfig);
+      }
+    } as TomlConfigService;
+    const filterList = new Set<string>(['-stellar', 'ASDF', 'usdx-asdf', 'asdf.net']);
+
+    //act
+    const assetStats = new AssetStatistics(new HorizonRestServiceStub() as HorizonRestService, tomlConfigServiceStub, filterList, 'USDX', 'GAMIFED');
+
+    //assert
+    expect(assetStats.assetIcon).toBe('/usdx.jpg');
+    expect(assetStats.hidden).toBeFalse();
+  });
+  
+  it('#constructor sets "hidden" to TRUE when asset full title found in filter-list', () => {
+    //arrange
+    const tomlConfigServiceStub = {
+      getIssuerConfig(tomlFileUrl: string) : Observable<IssuerConfiguration> {
+        const issuerConfig = {
+          currencies: [ {code: 'USDX', issuer: 'GAMIFED', image: '/usdx.jpg' } as TomlAsset ]
+        } as unknown as IssuerConfiguration;
+        return of(issuerConfig);
+      }
+    } as TomlConfigService;
+    const filterList = new Set<string>(['-stellar', 'ASDF', 'USDX-asd', 'asdf.net']);
+
+    //act
+    const assetStats = new AssetStatistics(new HorizonRestServiceStub() as HorizonRestService, tomlConfigServiceStub, filterList, 'USDX', 'GAMIFED');
+
+    //assert
+    expect(assetStats.assetIcon).toBe('/usdx.jpg');
+    expect(assetStats.hidden).toBeTrue();
   });
 });
 
